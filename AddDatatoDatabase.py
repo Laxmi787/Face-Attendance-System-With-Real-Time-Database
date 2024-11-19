cc3132391be69ac0cb400d5ba7dad325b83c919f
Laxmi chaudhary <166985345+Laxmi787@users.noreply.github.com>
2024-11-19 17:04:47 +0530

    Add files via upload

---
+++ AddDatatoDatabase.py
@@ -0,0 +1,46 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendancerealtime-bb63c-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "321654":
+        {
+            "name": "Laxmi Chaudhary",
+            "major": "IOT",
+            "starting_year": 2023,
+            "total_attendance": 10,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 11,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
